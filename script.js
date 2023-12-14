a1b3f412d6550da2bfcb6022dbf1e00dd8673d05
JoseAGomezBravo <04josegomez2022@gmail.com>
2023-12-13 18:03:45 -0800

    commit array

--- script.js
+++ script.js
@@ -2,4 +2,32 @@ let restartButton = document.getElementById("button-play-again")
 let gameBoxes = Array.from(document.getElementsByClassName("game-square"))
 let topText = document.getElementById("turn-tracker")
 
-console.log(gameBoxes)
+
+const O_Player = "O"
+const X_Player = "X"
+let currentPlayer = X_Player
+
+
+let boxSpaces = Array(9).fill(null)
+
+
+const gameStart = () => 
+{
+    gameBoxes.forEach(box => box.addEventListener("click", boxClick))
+}
+
+function boxClick(e)
+{
+    const id = e.target.id
+
+    if(!boxSpaces[id])
+    {
+        boxSpaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        currentPlayer = currentPlayer == X_Player ? O_Player : X_Player
+    }
+}
+
+gameStart()
+
