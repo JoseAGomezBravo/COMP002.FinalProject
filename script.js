519a761a7b8bfcbf0aa5937178ac7d38372e3f43
JoseAGomezBravo <04josegomez2022@gmail.com>
2023-12-13 19:27:48 -0800

    commit current turn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let restartButton = document.getElementById("button-play-again")
-let gameBoxes = Array.from(document.getElementsByClassName("game-square"))
-let topText = document.getElementById("turn-tracker")
-
+let restartButton = document.getElementById("button-play-again");
+let gameBoxes = Array.from(document.getElementsByClassName("game-square"));
+let topText = document.getElementById("title");
+const playerTurn = document.getElementById("turn");
 
 const O_Player = "O"
 const X_Player = "X"
@@ -9,6 +9,8 @@ let currentPlayer = X_Player
 
 
 let boxSpaces = Array(9).fill(null)
+
+let count_plays = 0
 
 
 const gameStart = () => 
@@ -20,27 +22,58 @@ function boxClick(e)
 {
     const id = e.target.id
 
-    if(!boxSpaces[id])
+    if(!boxSpaces[id] && count_plays < 9)
     {
         boxSpaces[id] = currentPlayer
         e.target.innerText = currentPlayer
 
-        if(playerWon())
-        {
-            topText = "${currentPlayer} has won!"
-        }
+        playerTurn.innerText = currentPlayer;
 
+        if(playerWon() !==false)
+        {
+            topText.innerText = currentPlayer + " Player Has Won!"
+
+
+            return
+            
+        }
+        count_plays++
         currentPlayer = currentPlayer == X_Player ? O_Player : X_Player
     }
+
+    if(count_plays === 9)
+    {
+        topText.innerHTML = "Draw Game!"
+    }
+
+    
+
+
 }
 
 
-
-
+const gameWin = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 
 function playerWon()
 {
+    for (const condition of gameWin) {
+        let [a,b,c] = condition
 
+        if(boxSpaces[a] && (boxSpaces[a] == boxSpaces[b] && boxSpaces[a] == boxSpaces[c])) 
+        {
+            return [a,b,c]
+        }
+    }
+    return false
 }
 
 
@@ -49,15 +82,16 @@ restartButton.addEventListener("click", restart)
 function restart()
 {
     boxSpaces.fill(null)
-
+    count_plays = 0
     gameBoxes.forEach( box =>
         {
             box.innerText =""
+            box.style.backgroundColor=""
         })
 
         currentPlayer = X_Player
 
-        topText = "Tic-Tac-Toe"
+        topText.innerHTML = "Tic-Tac-Toe"
 }
 
 gameStart()
