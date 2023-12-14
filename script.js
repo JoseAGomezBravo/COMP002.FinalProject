1cb5efcf03c56f62204306b9b61487749906ce30
JoseAGomezBravo <04josegomez2022@gmail.com>
2023-12-13 17:49:23 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let restartButton = document.getElementById("button-play-again")
+let gameBoxes = Array.from(document.getElementsByClassName("game-square"))
+let topText = document.getElementById("turn-tracker")
+
+console.log(gameBoxes)
